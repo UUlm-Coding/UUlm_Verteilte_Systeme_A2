3f73943c8c3faee63ee003346cf0f9cffe2d9162
JGlumanda <94634381+JGlumanda@users.noreply.github.com>
2024-10-18 14:16:41 +0200

    Aufgabe erledigt

--- src/main/java/de/uulm/verteilte_systeme/EchoClient.java
+++ src/main/java/de/uulm/verteilte_systeme/EchoClient.java
@@ -1,28 +1,38 @@
 package de.uulm.verteilte_systeme;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class EchoClient {
     private final String host;
     private final int port;
     private Socket socket;
-
+    private int timeoutDurationInMilliseconds = Integer.MAX_VALUE;
 
     public EchoClient(String host, int port) {
         this.host = host;
         this.port = port;
     }
 
-    public void connect() throws IOException {
-        socket = new Socket(host, port);
+    public EchoClient(String host, int port, int timoutDurationInMilliseconds) {
+        this(host, port);
+        this.timeoutDurationInMilliseconds = timoutDurationInMilliseconds;
+    }
+
+    public void connect() {
+        try {
+            socket = new Socket(host, port);
+            socket.setSoTimeout(timeoutDurationInMilliseconds);
+        } catch (IOException e) {
+            System.out.println("Could not connect to server: " + e.getMessage());
+        }
     }
 
     public String sendMessage(String message) {
-        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
-
         try {
             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader socketInputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -36,5 +46,13 @@ public class EchoClient {
             System.out.println("Exception: " + Arrays.toString(exception.getStackTrace()));
         }
         return "";
+    }
+
+    public void disconnect() {
+        try {
+            socket.close();
+        } catch (IOException exception) {
+            System.out.println("Exception: " + Arrays.toString(exception.getStackTrace()));
+        }
     }
 }
--- src/main/java/de/uulm/verteilte_systeme/Main.java
+++ src/main/java/de/uulm/verteilte_systeme/Main.java
@@ -1,84 +1,20 @@
 package de.uulm.verteilte_systeme;
 
-import java.io.IOException;
-import java.util.Stack;
-
 public class Main {
     public static void main(String[] args) {
-        Stack<Integer> numbers = new Stack<>();
-        Stack<String> operators = new Stack<>();
-        EchoClient client = new EchoClient("vs.lxd-vs.uni-ulm.de", 5678);
-
-        try {
-            client.connect();
-        } catch (IOException e) {
-            System.out.println("Could not connect to server: " + e.getMessage());
-            return;
-        }
+        EchoClient client = new EchoClient("vs.lxd-vs.uni-ulm.de", 5678, 5000);
+        client.connect();
 
         String response = client.sendMessage("Please provide a new expression!");
         System.out.println("Server response: " + response);
 
-        for (int i = 0; i < response.length(); i++) {
-            String currentChar = String.valueOf(response.charAt(i));
+        int result = ExpressionEvaluator.evaluateExpression(response);
+        System.out.println("Result: " + result);
 
-            if (isNumeric(currentChar)) {
-                numbers.push(Integer.parseInt(currentChar));
-            } else if (isOperator(currentChar)){
-                if (!hasPriority(currentChar)){
-                    operators.push(currentChar);
-                }else {
-                    int a = numbers.pop();
-                    int b = Integer.parseInt(String.valueOf(response.charAt(i + 1)));
-                    numbers.push(calculate(a, b, currentChar));
-                    i ++;
-                }
-            }
-        }
-
-        while (!operators.isEmpty()){
-            int a = numbers.pop();
-            int b = numbers.pop();
-            numbers.push(calculate(a, b, operators.pop()));
-        }
-
-        System.out.println("Result: " + numbers.peek());
-
-        String resultString = String.format("%s=%d", response,numbers.peek());
-
+        String resultString = String.format("%s=%d", response, result);
         response = client.sendMessage(resultString);
-
         System.out.println("Server response: " + response);
-    }
 
-    public static boolean isNumeric(String strNum) {
-        if (strNum == null) {
-            return false;
-        }
-        try {
-            Double.parseDouble(strNum);
-        } catch (NumberFormatException nfe) {
-            return false;
-        }
-        return true;
-    }
-
-    public static boolean isOperator(String strOp) {
-        if (strOp == null) {
-            return false;
-        }
-        return strOp.equals("+") || strOp.equals("*");
-    }
-
-    public static boolean hasPriority(String op){
-        return op.equals("*");
-    }
-
-    public static int calculate(int a, int b, String operator) {
-        return switch (operator) {
-            case "+" -> a + b;
-            case "*" -> a * b;
-            default -> 0;
-        };
+        client.disconnect();
     }
 }
